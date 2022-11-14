090c9b1f632130dd081dfbda24ebf00fe81011e3
Liewie <lucascpbarbosa0@gmail.com>
2022-11-14 19:03:04 -0300

    .

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const robo = document.querySelector('.robo')
 const somar = document.querySelector('#somar')
 const subtrair = document.querySelector('#subtrair')
-const braco = document.querySelector('#braco')
-const controles = document.querySelectorAll('.controle-ajuste')
+const controles = document.querySelectorAll('[data-controle]')
 
-controles.forEach((elemento) =>{
+
+//forEach procura por todos os elementos dentro de um array. É necessário passar uma função, sendo o primeiro parametro o elemento em questao e o segundo parametro o seu index.
+controles.forEach((elemento) => {
+    //Adicionamos o evento de click em cada um dos elementos
     elemento.addEventListener('click', (evento) =>{
-        console.log(elemento)
-        console.log(evento)
+        //Evento é o que ocorre quando aquele elemento é clicado, 
+        manipularValores(evento.target.dataset.controle, evento.target.parentNode)
+
     })
 })
+//A função recebe dois parametros, sendo qual operacao será feita e o outro de qual elemento estamos falando.
+function manipularValores(operacao, controle){
+    //Selecionamos o controle-contador especifico (graças ao pai ter sido selecionado na momento em que a funcao foi chamada)
+    const peca = controle.querySelector('[data-peca]')
 
-function manipularValores(operacao){
     if(operacao === '+'){
-        var abc = Number(braco.value) + 1
-        braco.value = abc
+        var abc = Number(peca.value) + 1
+        peca.value = abc
     }
     else{
-        braco.value = Number(braco.value) - 1
+        peca.value = Number(peca.value) - 1
     }
 }
+
+
 //Arrow function é usada para poucos (ou únicos) comandos. Caso tenha mais de uma linha de código, usar a função anônima.
 robo.addEventListener('click', (evento) =>{alert('Oi!'); console.log(evento)})
